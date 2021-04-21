4d9aba51d2a45795094ae44c9d143f5fb6fade1c
Jasmine Kay <jasminekay505@gmail.com>
2021-04-21 09:35:45 -0600

    Adding back in starter files

--- public/index.js
+++ public/index.js
@@ -1,104 +1,14 @@
-let db;
-let budgetVersion;
-
-// Create a new db request for a "budget" database.
-const request = indexedDB.open('BudgetDB', budgetVersion || 21);
-
-request.onupgradeneeded = function (e) {
-  console.log('Upgrade needed in IndexDB');
-
-  const { oldVersion } = e;
-  const newVersion = e.newVersion || db.version;
-
-  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-
-  db = e.target.result;
-
-  if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
-  }
-};
-
-request.onerror = function (e) {
-  console.log(`Woops! ${e.target.errorCode}`);
-};
-
-function checkDatabase() {
-  console.log('check db invoked');
-
-  // Open a transaction on your BudgetStore db
-  let transaction = db.transaction(['BudgetStore'], 'readwrite');
-
-  // access your BudgetStore object
-  const store = transaction.objectStore('BudgetStore');
-
-  // Get all records from store and set to a variable
-  const getAll = store.getAll();
-
-  // If the request was successful
-  getAll.onsuccess = function () {
-    // If there are items in the store, we need to bulk add them when we are back online
-    if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => response.json())
-        .then((res) => {
-          // If our returned response is not empty
-          if (res.length !== 0) {
-            // Open another transaction to BudgetStore with the ability to read and write
-            transaction = db.transaction(['BudgetStore'], 'readwrite');
-
-            // Assign the current store to a variable
-            const currentStore = transaction.objectStore('BudgetStore');
-
-            // Clear existing entries because our bulk add was successful
-            currentStore.clear();
-            console.log('Clearing store 🧹');
-          }
-        });
-    }
-  };
-}
-
-request.onsuccess = function (e) {
-  console.log('success');
-  db = e.target.result;
-
-  // Check if app is online before reading from db
-  if (navigator.onLine) {
-    console.log('Backend online! 🗄️');
-    checkDatabase();
-  }
-};
-
-const saveRecord = (record) => {
-  console.log('Save record invoked');
-  // Create a transaction on the BudgetStore db with readwrite access
-  const transaction = db.transaction(['BudgetStore'], 'readwrite');
-
-  // Access your BudgetStore object store
-  const store = transaction.objectStore('BudgetStore');
-
-  // Add record to your store with add method.
-  store.add(record);
-};
-
-// Listen for app coming back online
-window.addEventListener('online', checkDatabase);
 let transactions = [];
 let myChart;
 
-fetch('/api/transaction')
-  .then((response) => response.json())
-  .then((data) => {
+fetch("/api/transaction")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
     // save db data on global variable
     transactions = data;
+
     populateTotal();
     populateTable();
     populateChart();
@@ -106,23 +16,21 @@ fetch('/api/transaction')
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  const total = transactions
-    .reduce((total, t) => {
-      return total + parseFloat(t.value);
-    }, 0)
-    .toFixed(2);
+  let total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
 
-  const totalEl = document.querySelector('#total');
+  let totalEl = document.querySelector("#total");
   totalEl.textContent = total;
 }
 
 function populateTable() {
-  const tbody = document.querySelector('#tbody');
-  tbody.innerHTML = '';
+  let tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
 
-  transactions.forEach((transaction) => {
+  transactions.forEach(transaction => {
     // create and populate a table row
-    const tr = document.createElement('tr');
+    let tr = document.createElement("tr");
     tr.innerHTML = `
       <td>${transaction.name}</td>
       <td>${transaction.value}</td>
@@ -134,17 +42,17 @@ function populateTable() {
 
 function populateChart() {
   // copy array and reverse it
-  const reversed = transactions.slice().reverse();
+  let reversed = transactions.slice().reverse();
   let sum = 0;
 
   // create date labels for chart
-  const labels = reversed.map((t) => {
-    const date = new Date(t.date);
+  let labels = reversed.map(t => {
+    let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
   // create incremental values for chart
-  const data = reversed.map((t) => {
+  let data = reversed.map(t => {
     sum += parseInt(t.value);
     return sum;
   });
@@ -154,42 +62,41 @@ function populateChart() {
     myChart.destroy();
   }
 
-  const ctx = document.getElementById('my-chart').getContext('2d');
+  let ctx = document.getElementById("myChart").getContext("2d");
 
   myChart = new Chart(ctx, {
     type: 'line',
-    data: {
-      labels,
-      datasets: [
-        {
-          label: 'Total Over Time',
-          fill: true,
-          backgroundColor: '#6666ff',
-          data,
-        },
-      ],
-    },
+      data: {
+        labels,
+        datasets: [{
+            label: "Total Over Time",
+            fill: true,
+            backgroundColor: "#6666ff",
+            data
+        }]
+    }
   });
 }
 
 function sendTransaction(isAdding) {
-  const nameEl = document.querySelector('#t-name');
-  const amountEl = document.querySelector('#t-amount');
-  const errorEl = document.querySelector('form .error');
+  let nameEl = document.querySelector("#t-name");
+  let amountEl = document.querySelector("#t-amount");
+  let errorEl = document.querySelector(".form .error");
 
   // validate form
-  if (nameEl.value === '' || amountEl.value === '') {
-    errorEl.textContent = 'Missing Information';
+  if (nameEl.value === "" || amountEl.value === "") {
+    errorEl.textContent = "Missing Information";
     return;
-  } else {
-    errorEl.textContent = '';
+  }
+  else {
+    errorEl.textContent = "";
   }
 
   // create record
-  const transaction = {
+  let transaction = {
     name: nameEl.value,
     value: amountEl.value,
-    date: new Date().toISOString(),
+    date: new Date().toISOString()
   };
 
   // if subtracting funds, convert amount to negative number
@@ -204,43 +111,43 @@ function sendTransaction(isAdding) {
   populateChart();
   populateTable();
   populateTotal();
-
+  
   // also send to server
-  fetch('/api/transaction', {
-    method: 'POST',
+  fetch("/api/transaction", {
+    method: "POST",
     body: JSON.stringify(transaction),
     headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
   })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.errors) {
-        errorEl.textContent = 'Missing Information';
-      } else {
-        // clear form
-        nameEl.value = '';
-        amountEl.value = '';
-      }
-    })
-    .catch((err) => {
-      // fetch failed, so save in indexed db
-      console.log('save record');
-      saveRecord(transaction);
-
+  .then(response => {    
+    return response.json();
+  })
+  .then(data => {
+    if (data.errors) {
+      errorEl.textContent = "Missing Information";
+    }
+    else {
       // clear form
-      nameEl.value = '';
-      amountEl.value = '';
-    });
+      nameEl.value = "";
+      amountEl.value = "";
+    }
+  })
+  .catch(err => {
+    // fetch failed, so save in indexed db
+    saveRecord(transaction);
+
+    // clear form
+    nameEl.value = "";
+    amountEl.value = "";
+  });
 }
 
-document.querySelector('#add-btn').addEventListener('click', function (event) {
-  event.preventDefault();
+document.querySelector("#add-btn").onclick = function() {
   sendTransaction(true);
-});
+};
 
-document.querySelector('#sub-btn').addEventListener('click', function (event) {
-  event.preventDefault();
+document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
-});
+};
